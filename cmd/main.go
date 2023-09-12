117f2bafd01eb3bf4cbcbe5bbc605c8a48789772
Sarwesh Maharjan <sarweshmaharjan@lftechnology.com>
2023-09-12 23:28:46 +0545

    add: env config and ci cd workflow

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,20 @@
 package main
 
+import (
+	"fmt"
+	"log"
+
+	"github.com/sarweshmaharjan/api-simulator.git/internal/config"
+)
+
 func main() {
-	
+	cfg, err := config.LoadConfig()
+	if err != nil {
+		log.Fatalf("Error loading configuration: %v", err)
+	}
+
+	fmt.Printf("API URL: %s\n", cfg.APIURL)
+	fmt.Printf("Port: %s\n", cfg.Port)
+
+	// Use other configuration fields as needed
 }
---
+++ internal/config/config.go
@@ -0,0 +1,28 @@
+package config
+
+import (
+	"github.com/joho/godotenv"
+	"log"
+	"os"
+)
+
+type Config struct {
+	APIURL string
+	Port   string
+	// Add more configuration fields here
+}
+
+func LoadConfig() (*Config, error) {
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Fatalf("Error loading .env file: %v", err)
+	}
+
+	config := &Config{
+		APIURL: os.Getenv("API_URL"),
+		Port:   os.Getenv("PORT"),
+		// Initialize other configuration fields here
+	}
+
+	return config, nil
+}
